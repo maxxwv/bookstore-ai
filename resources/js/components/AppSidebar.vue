160922919ef09ea6a3ebae3bc5668a190fdba214
Alex Wharff <git@maxxwv.com>
2026-02-08 18:14:22 -0500

    Cleaned up some of the default starter kit stuff

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -13,14 +13,14 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { dashboard } from '@/routes';
+import { conversationForm } from '@/routes';
 import { type NavItem } from '@/types';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
     {
-        title: 'Dashboard',
-        href: dashboard(),
+        title: 'Conversation ',
+        href: conversationForm(),
         icon: LayoutGrid,
     },
 ];
@@ -45,7 +45,7 @@ const footerNavItems: NavItem[] = [
             <SidebarMenu>
                 <SidebarMenuItem>
                     <SidebarMenuButton size="lg" as-child>
-                        <Link :href="dashboard()">
+                        <Link :href="conversationForm()">
                             <AppLogo />
                         </Link>
                     </SidebarMenuButton>
@@ -59,7 +59,7 @@ const footerNavItems: NavItem[] = [
 
         <SidebarFooter>
             <NavFooter :items="footerNavItems" />
-            <NavUser />
+            <!-- <NavUser /> -->
         </SidebarFooter>
     </Sidebar>
     <slot />
--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,8 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return redirect()->route('conversation-form');
 })->name('home');
-
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/settings.php';
 
 Route::prefix('chatbot')->group(function() {
 	Route::get('/', [AiConversationController::class, 'conversationForm'])->name('conversation-form');
